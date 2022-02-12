309e6c36ef394b82bae7be7c0b0756b1dee24841
Aakash Singh <42798540+akash2140@users.noreply.github.com>
2022-02-12 11:49:25 +0530

    Add files via upload
    Latest Spring version 2.6.3 has been used.
    All the dependencies has been added in pom.xml file.
    Just import it as maven project and update maven and you are good to go.
    Discover services using eureka.

---
+++ MyEurekaServer/src/test/java/com/akash/MyEurekaServer/MyEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akash.MyEurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
